65d7d2d04d599ae57a2f41bb9bcd00de50edd505
Philippe Le Rohellec <philippe@lerohellec.com>
2026-01-06 15:54:08 -0800

    use playwright-mcp instead of custom playwright integration in test script

---
+++ bin/playwright-mcp.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require 'dotenv/load'
+require 'ruby_llm/mcp'
+
+class LogFormatter
+  TIME_FORMAT = '%H:%M:%S'
+  def call(severity, datetime, progname, msg)
+    "#{datetime.strftime(TIME_FORMAT)} #{severity[0]}: #{msg}\n"
+  end
+end
+
+logger = Logger.new(STDOUT)
+logger.formatter = LogFormatter.new
+logger.level = Logger::INFO
+
+provider = 'openrouter'
+model = 'google/gemini-2.5-flash-preview-09-2025'
+
+RubyLLM.configure do |config|
+  config.openrouter_api_key = ENV['OPENROUTER_API_KEY']
+  config.gemini_api_key = ENV['GEMINI_API_KEY']
+  config.default_model = model
+
+  config.logger = logger
+end
+
+client = RubyLLM::MCP.client(
+  name: "playwright-mcp",
+  transport_type: :streamable,
+  config: {
+      url: "http://localhost:8931/mcp",
+  },
+)
+
+tools = client.tools
+
+puts "Available tools:"
+tools.each do |tool|
+  puts "- #{tool.name}: #{tool.description}"
+end
+
+
+chat = RubyLLM.chat(model:, provider:)
+chat.with_tools(*client.tools)
+
+response = chat.ask("What's the title of https://www.vpsbenchmarks.com ? Use the Playwright tool to navigate to the page and extract the title.")
+puts response.content
